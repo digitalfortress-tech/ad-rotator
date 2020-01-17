b2eab788176ecdee1cbd972cf841440c2cd0a8b8
Niket Pathak <niketpathak89@gmail.com>
2020-01-17 14:40:35 +0100

    allow random rotation of ads

--- src/ad-rotator.js
+++ src/ad-rotator.js
@@ -6,34 +6,50 @@
 function getDefaultConfig(shape = 'square') {
   let config = {
     shape: 'square',
-    height: 400,
-    width: 400,
+    height: 300,
+    width: 250,
     padding: 0,
     margin: 0,
-    sticky: false
+    sticky: false,
+    timer: 10000,
+    random: true,
+    debug: false
   };
-  if (shape === 'rectangle-horizontal') {
-    config.height = 200;
-    config.width = 600;
-    return config;
-  }
-  if (shape === 'rectangle-vertical') {
+  if (shape === 'leaderboard') {
+    config.height = 90;
+    config.width = 728;
+  } else if (shape === 'sidebar') {
     config.height = 600;
-    config.width = 200;
-    return config;
+    config.width = 300;
   }
 
   return  config;
 }
 
-export default function (units = {}, options = {}) {
-  const {sticky, shape, height, width, padding, margin} = Object.assign({}, getDefaultConfig(), options);
-
-
-  return {
-    test() {
-      console.log('---->>>>', sticky, shape, height, width, padding, margin);
-    }
-  }
+function rotateImage(El, units, conf) {
+  const randomUnit = units.length === 1 ? units[0] : units[Math.floor(Math.random() * (units.length - 1 + 1))];
+  // console.debug('***rotateImg', randomUnit, units, conf);
+  let img = new Image(conf.height, conf.width);
+  img.src = randomUnit.img;
+  El.childNodes[0] ? El.replaceChild(img, El.childNodes[0]) : El.appendChild(img);
 }
 
+export default function (htmlEl, units = [], options = {}) {
+  const conf = Object.assign({}, getDefaultConfig(), options);
+  if (!htmlEl || !htmlEl instanceof HTMLElement || !units || !units instanceof Array || !units.length || !units[0] instanceof Object || !units[0].url || !units[0].img) {
+    conf.debug && console.error('Missing/malformed parameters. Html El, Ad Units', htmlEl, units);
+    return false;
+  }
+  rotateImage(htmlEl, units, conf);
+  window.setInterval(rotateImage, conf.timer, htmlEl, units, conf);
+
+
+
+
+  // console.debug('***htmlEl', htmlEl, htmlEl instanceof HTMLElement);
+
+  return true;
+}
+
+
+// @todo: add support for inline encoded images too
