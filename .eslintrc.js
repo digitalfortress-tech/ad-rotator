a7358b50eff4d7d6b668a5f55c9107b01c18976b
Niket Pathak <niketpathak89@gmail.com>
2020-09-20 20:24:56 +0200

    Update eslint config to support promises/async-await

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,25 @@
 module.exports = {
-    "extends": "eslint:recommended",
-    "env": {
-      "browser": true,
-      "node": true,
-    },
-    "parserOptions": {
-      "sourceType": "module",
-      "ecmaVersion": 2015
-    },
-    "rules": {
-        // enable additional rules
-        "indent": ["error", 2],
-        "linebreak-style": ["error", "unix"],
-        "quotes": ["error", "double"],
-        "semi": ["error", "always"],
+  extends: "eslint:recommended",
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2017,
+  },
+  rules: {
+    // enable additional rules
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
 
-        // override default options for rules from base configurations
-        "no-cond-assign": ["error", "always"],
+    // override default options for rules from base configurations
+    "no-cond-assign": ["error", "always"],
 
-        // disable rules from base configurations
-        "no-console": "off",
-    }
-}
+    // disable rules from base configurations
+    "no-console": "off",
+  },
+};
