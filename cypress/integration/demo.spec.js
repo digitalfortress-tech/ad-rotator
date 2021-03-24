42bb7a87d19a4cf1a6db599c0993b0390999b356
Niket Pathak <niketpathak89@gmail.com>
2021-03-24 19:27:24 +0100

    test: add cypress test to verify rotation and hover

--- cypress/integration/demo.spec.js
+++ cypress/integration/demo.spec.js
@@ -1,6 +1,38 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
 describe('AdRotator', () => {
-  it('Does not do much!', () => {
+  beforeEach(() => {
+    cy.viewport(1200, 1200);
+  });
+  it('should rotate Ad correctly (sequentially)', () => {
     cy.visit('/demo/index.html');
-    expect(true).to.equal(true);
+    cy.get('#sidebar-placement-1').as('sidebarAd').find('a').should('have.attr', 'href', 'https://niketpathak.com#1');
+    cy.get('@sidebarAd').find('img').should('have.attr', 'src', './assets/images/square/1.jpg');
+    cy.wait(1000);
+    cy.get('@sidebarAd').find('a').should('have.attr', 'href', 'https://digitalfortress.tech#2');
+    cy.get('@sidebarAd').find('img').should('have.attr', 'src', './assets/images/square/2.jpg');
+    cy.wait(1000);
+    cy.get('@sidebarAd').find('a').should('have.attr', 'href', 'https://digitalfortress.tech#3');
+    cy.get('@sidebarAd').find('img').should('have.attr', 'src', './assets/images/square/3.jpg');
+  });
+
+  it('should pause rotation on hover', () => {
+    cy.visit('/demo/index.html');
+    cy.get('#sidebar-placement-1').as('sidebarAd').find('a').should('have.attr', 'href', 'https://niketpathak.com#1');
+    cy.get('@sidebarAd').find('img').should('have.attr', 'src', './assets/images/square/1.jpg');
+    cy.get('@sidebarAd').trigger('mouseenter');
+    cy.wait(3100);
+    cy.get('@sidebarAd').find('img').should('have.attr', 'src', './assets/images/square/1.jpg');
+  });
+
+  xit('should be visible only on desktop', () => {
+    // bug in cypress doesn't retrieve the correct "window.screen.availWidth" even after setting the viewport
+    cy.visit('/demo/index.html');
+    cy.get('#hz-placement-1').as('leaderboardAd').find('a').should('have.attr', 'href', 'https://gospelmusic.io#5');
+    cy.get('@leaderboardAd')
+      .find('img')
+      .should('have.attr', 'src', 'https://niketpathak.com/images/works/gkm_pic_sq.jpg');
+    cy.viewport(400, 400);
+    cy.reload(true);
+    cy.get('#hz-placement-1').find('a').should('not.exist');
   });
 });
